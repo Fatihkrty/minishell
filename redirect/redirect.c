beed8dd63554735922cb3f95650576fa2cfd5f37
Fatih Karatay <karatayy61@gmail.com>
2022-10-03 16:56:39 +0300

    Heredoc çıktı görünmemesi düzeltildi

--- redirect/redirect.c
+++ redirect/redirect.c
@@ -8,16 +8,39 @@ void	run_redirects(t_process *process, int heredoc)
 
 	i = 0;
 	redirects = process->redirects;
-	while (redirects[i])
+	if (heredoc)
 	{
-		if (is_operator(redirects[i]) == RED_OUTPUT)
-			red_output(redirects[i + 1], REPLACE);
-		else if (is_operator(redirects[i]) == RED_APPEND)
-			red_output(redirects[i + 1], APPEND);
-		else if (is_operator(redirects[i]) == RED_INPUT)
-			red_input(redirects[i + 1]);
-		else if (is_operator(redirects[i]) == HERE_DOC && heredoc)
-			red_heredoc(redirects[i + 1]);
-		i++;
+		while (redirects[i])
+		{
+			if (is_operator(redirects[i]) == RED_INPUT)
+				red_input(redirects[i + 1]);
+			else if (is_operator(redirects[i]) == HERE_DOC && heredoc)
+				red_heredoc(redirects[i + 1]);
+			i++;
+		}
+		i = 0;
+		while (redirects[i])
+		{
+			if (is_operator(redirects[i]) == RED_OUTPUT)
+				red_output(redirects[i + 1], REPLACE);
+			if (is_operator(redirects[i]) == RED_APPEND)
+				red_output(redirects[i + 1], APPEND);
+			i++;
+		}
+	}
+	else
+	{
+		i = 0;
+		while (redirects[i])
+		{
+			if (is_operator(redirects[i]) == RED_OUTPUT)
+				red_output(redirects[i + 1], REPLACE);
+			else if (is_operator(redirects[i]) == RED_APPEND)
+				red_output(redirects[i + 1], APPEND);
+			if (is_operator(redirects[i]) == RED_INPUT)
+				red_input(redirects[i + 1]);
+			i++;
+		}
+
 	}
 }

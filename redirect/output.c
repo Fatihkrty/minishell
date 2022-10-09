ad4bbc1e73f1b8152b25f2c67b81b4d4ddffc4d4
Fatih Karatay <karatayy61@gmail.com>
2022-10-09 21:26:40 +0300

    echo redirect fixlendi

--- redirect/output.c
+++ redirect/output.c
@@ -8,20 +8,14 @@ void	output(t_process *process, char *file, int mode)
 		fd = open(file, O_CREAT | O_WRONLY | O_TRUNC, 0777);
 	else if (mode == APPEND)
 		fd = open(file, O_CREAT | O_WRONLY | O_APPEND, 0777);
-	if (is_parent())
+	if (fd == -1)
 	{
-		if (fd == -1)
-			return perror("minishell");
-		dup2(fd, process->fd[1]);
-	}
-	else
-	{
-		if (fd == -1)
-		{
-			perror("minishell");
+		perror("minishell");
+		if (is_parent())
+			return;
+		else
 			exit(errno);
-		}
-		dup2(fd, 1);
 	}
+	dup2(fd, 1);
 	close(fd);
 }
